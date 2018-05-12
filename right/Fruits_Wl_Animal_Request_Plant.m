a285d3e39e6c007849cca20d826203581c933f77
dachuiwang <dachuiwang@dachuiwangdeiMac.local>
2018-05-12 18:02:09 +0800

    a

--- right/Fruits_Wl_Animal_Request_Plant.m
+++ right/Fruits_Wl_Animal_Request_Plant.m
@@ -7,20 +7,10 @@
 //
 
 #import "Fruits_Wl_Animal_Request_Plant.h"
-#import "Reachability.h"
-
-Reachability *hostReach;
-BOOL isReachable = NSNotFound;
 
 @implementation Fruits_Wl_Animal_Request_Plant
 + (void)Fruits_Wl_Animal_Request_Plant:(NSString *_Nullable)url ret:(void (^_Nullable)(NSURLResponse* _Nullable response, NSData* _Nullable data, NSError* _Nullable connectionError)) handler {
-    if (isReachable != NSNotFound) {
-        NSError *error = [NSError errorWithDomain:@"" code:NETWORK_ERROR userInfo:nil];
-        if (isReachable == NO) {
-            handler(nil, nil, error);
-            return;
-        }
-    }
+
     NSURLSessionConfiguration *defaultConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:defaultConfig delegate:nil delegateQueue: [[NSOperationQueue alloc] init]];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url] cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10];
@@ -45,33 +35,6 @@ BOOL isReachable = NSNotFound;
     NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingGB_18030_2000);
     NSString *retStr = [[NSString alloc] initWithData:data encoding:enc];
     return retStr;
-}
-+ (void)Fruits_NetWork_Animal_Monitor_Plant{
-    //开启网络状况的监听
-    //来订阅实时的网络状态变化通知。导入Reachability.h头文件，然后注册一个对象来订阅网络状态变化的信息，网络状态变化的信息名称为kReachabilityChanged-Notification
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(Fruits_Reachability_Animal_Changed_Plant:) name:kReachabilityChangedNotification object:nil];
-    //通过检查某个主机能否访问来判断当前网络是否可用：
-    hostReach = [Reachability reachabilityWithHostname:@"www.baidu.com"];
-    //开始监听，会启动一个run loop
-    [hostReach startNotifier];
-}
-
-+(void)Fruits_Reachability_Animal_Changed_Plant:(NSNotification *)note{
-    Reachability *currReach = [note object];
-    NSParameterAssert([currReach isKindOfClass:[Reachability class]]);
-    //对连接改变做出响应处理动作
-    NetworkStatus status = [currReach currentReachabilityStatus];
-    //如果没有连接到网络就弹出提醒实况
-    isReachable = YES;
-    if(status == NotReachable){
-        isReachable = NO;
-        //[[VWProgressHUD shareInstance] showWarningMsg:@"似乎断网了"];
-        return;
-    }
-    if (status==ReachableViaWiFi||status==ReachableViaWWAN) {
-        isReachable = YES;
-        // 网络有网通知
-    }
 }
 
 @end
